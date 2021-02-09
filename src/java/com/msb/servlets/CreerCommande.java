e6f4ce52e27bc74d583b6d4d852d4d7fffaa2fcc
Samy <samsixhuit@protonmail.com>
2021-02-09 17:38:14 +0100

    créé et affiche la date de commande au bon format

--- src/java/com/msb/servlets/CreerCommande.java
+++ src/java/com/msb/servlets/CreerCommande.java
@@ -6,10 +6,12 @@
 package com.msb.servlets;
 
 import java.io.IOException;
+import java.util.Locale;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.joda.time.DateTime;
 
 /**
  *
@@ -29,6 +31,11 @@ public class CreerCommande extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        // créé une date
+        DateTime dt = new DateTime();
+        String date = dt.toString("dd/MM/yyyy HH:mm:ss");
+        request.setAttribute("date", date);
+        this.getServletContext().getRequestDispatcher("/afficherCommande.jsp")
+                .forward(request, response);
     }
 }
